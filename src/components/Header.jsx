41e42dc126e43545c99d2fab3cd28445d2c86df0
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-03-05 14:41:50 +0500

    Added some components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useLocation } from "react-router-dom";
-// import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 import { brainwave } from "../assets";
-import { navigation } from "../constants"; 
+import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
