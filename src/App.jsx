bfa6e8e58adff4a3dadc3067f40886686cb38ab3
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-03-05 11:51:30 +0500

    Added some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 
 const App = () => {
@@ -8,10 +9,10 @@ const App = () => {
     <>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
 
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
-      <Button className="mt-10" href="#login">
-          Something
-      </Button>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      
       <ButtonGradient />
     </>
   );
