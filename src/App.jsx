d9dd0dbbf7eea9c1555a21307049e55df015379a
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2025-03-06 11:59:11 +0500

    Added a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
 import Header from "./components/Header";
-import Section from "./components/Section";
 import Hero from "./components/Hero";
+import Benefits from "./components/Benefits"
 
 
 const App = () => {
   return (
     <>
-      {/* <h1 class="text-3xl font-bold underline">Hello world!</h1> */}
 
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Section />
         <Hero />
+        <Benefits />
       </div>
       
       <ButtonGradient />
